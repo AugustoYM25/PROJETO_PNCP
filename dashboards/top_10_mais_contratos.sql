73485ff81c65d9e5c10730e67dfe2a75aa9322c9
Augusto <yoshikawaaugusto@gmail.com>
2026-04-12 13:14:40 -0300

    Delete dashboards/top_10_mais_contratos.sql

--- dashboards/top_10_mais_contratos.sql
+++
@@ -1,6 +0,0 @@
-SELECT TOP 10
-    orgaoEntidade_razaoSocial AS Orgao,
-    COUNT(numeroControlePncpCompra) AS Quantidade_Contratos
-FROM silver.contratos
-GROUP BY orgaoEntidade_razaoSocial
-ORDER BY Quantidade_Contratos DESC;
