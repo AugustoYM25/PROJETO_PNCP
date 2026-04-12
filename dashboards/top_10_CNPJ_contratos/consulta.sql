242a733b433a8c6b2600a0dec74883c861cf6662
Augusto <yoshikawaaugusto@gmail.com>
2026-04-12 13:53:33 -0300

    Update consulta.sql

--- dashboards/top_10_CNPJ_contratos/consulta.sql
+++ dashboards/top_10_CNPJ_contratos/consulta.sql
@@ -1,1 +1,8 @@
-
+SELECT TOP 10
+    nomeRazaoSocialFornecedor AS Razao_Social,
+    niFornecedor AS CNPJ_Fornecedor,  
+    COUNT(numeroControlePncpCompra) AS Total_Contratos
+FROM silver.contratos
+WHERE niFornecedor IS NOT NULL
+GROUP BY nomeRazaoSocialFornecedor,niFornecedor
+ORDER BY Total_Contratos DESC;
