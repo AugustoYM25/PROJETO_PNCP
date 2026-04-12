3be9dadb9c855ca916c8f59345655908ed7ee2e2
Augusto <yoshikawaaugusto@gmail.com>
2026-04-12 13:12:37 -0300

    Update top_10_mais_contratos.sql

--- dashboards/top_10_mais_contratos.sql
+++ dashboards/top_10_mais_contratos.sql
@@ -1,1 +1,6 @@
-
+SELECT TOP 10
+    orgaoEntidade_razaoSocial AS Orgao,
+    COUNT(numeroControlePncpCompra) AS Quantidade_Contratos
+FROM silver.contratos
+GROUP BY orgaoEntidade_razaoSocial
+ORDER BY Quantidade_Contratos DESC;
